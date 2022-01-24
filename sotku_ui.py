c12b765a1b4d7da582d3af0d3ba5f290e294c754
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-01-24 13:44:54 +0200

    quiz app start

---
+++ sotku_ui.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'sotku.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QListView, QMainWindow, QMenuBar,
+    QRadioButton, QSizePolicy, QSlider, QStatusBar,
+    QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.listView = QListView(self.centralwidget)
+        self.listView.setObjectName(u"listView")
+        self.listView.setGeometry(QRect(370, 50, 157, 192))
+        self.verticalLayoutWidget = QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(120, 50, 211, 245))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.radioButton = QRadioButton(self.verticalLayoutWidget)
+        self.radioButton.setObjectName(u"radioButton")
+
+        self.verticalLayout.addWidget(self.radioButton)
+
+        self.radioButton_3 = QRadioButton(self.verticalLayoutWidget)
+        self.radioButton_3.setObjectName(u"radioButton_3")
+
+        self.verticalLayout.addWidget(self.radioButton_3)
+
+        self.radioButton_2 = QRadioButton(self.verticalLayoutWidget)
+        self.radioButton_2.setObjectName(u"radioButton_2")
+
+        self.verticalLayout.addWidget(self.radioButton_2)
+
+        self.horizontalSlider = QSlider(self.centralwidget)
+        self.horizontalSlider.setObjectName(u"horizontalSlider")
+        self.horizontalSlider.setGeometry(QRect(130, 330, 191, 21))
+        self.horizontalSlider.setOrientation(Qt.Horizontal)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 800, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.radioButton.setText(QCoreApplication.translate("MainWindow", u"RadioButton", None))
+        self.radioButton_3.setText(QCoreApplication.translate("MainWindow", u"RadioButton", None))
+        self.radioButton_2.setText(QCoreApplication.translate("MainWindow", u"RadioButton", None))
+    # retranslateUi
+
